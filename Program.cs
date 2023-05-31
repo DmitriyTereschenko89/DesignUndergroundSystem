31914efef83240eee05330125e0afc0998ae2ef4
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-05-31 11:02:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignUndergroundSystem
+{
+	internal class Program
+	{
+		public class UndergroundSystem
+		{
+
+			public UndergroundSystem()
+			{
+
+			}
+
+			public void CheckIn(int id, string stationName, int t)
+			{
+
+			}
+
+			public void CheckOut(int id, string stationName, int t)
+			{
+
+			}
+
+			public double GetAverageTime(string startStation, string endStation)
+			{
+
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
